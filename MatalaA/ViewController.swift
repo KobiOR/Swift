5018751387039a06c27635b2504eb31fc7726447
Kobi Ohana <ohanako@bmw24.cs.colman.ac.il>
2016-11-23 14:22:25 +0200

    Initial Commit

---
+++ MatalaA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MatalaA
+//
+//  Created by Kobi Ohana on 23/11/2016.
+//  Copyright © 2016 Kor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
