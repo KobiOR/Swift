19b7344673546dfc3e731a10dca16552a4dd6957
KobiOR <admin@IMGOSX.local>
2016-11-25 18:21:49 +0200

    update

--- MatalaATests/MatalaATests.swift
+++ MatalaATests/MatalaATests.swift
@@ -10,9 +10,14 @@ import XCTest
 
 class MatalaATests: XCTestCase {
     
+    let studentsDb=StudentDataBase()
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        studentsDb.addStudent(firstName: "A", lastName: "A", idStudent: "200435865", phoneNumber: "0503520007")
+        studentsDb.addStudent(firstName: "B", lastName: "B", idStudent: "2004358652", phoneNumber: "05035200072")
+        studentsDb.addStudent(firstName: "C", lastName: "C", idStudent: "2004358653", phoneNumber: "05035200073")
+        studentsDb.addStudent(firstName: "D", lastName: "D", idStudent: "2004358654", phoneNumber: "05035200074")
     }
     
     override func tearDown() {
@@ -22,28 +27,76 @@ class MatalaATests: XCTestCase {
     
     func testExample() {
         
-        let studentsDb=StudentDataBase()
-        studentsDb.addStudent(firstName: "Kobi", lastName: "Or", idStudent: "200435865", phoneNumber: "0503520007")
-       studentsDb.addStudent(firstName: "Kobi2", lastName: "Or2", idStudent: "2004358652", phoneNumber: "05035200072")
-       studentsDb.addStudent(firstName: "Kobi3", lastName: "Or3", idStudent: "2004358653", phoneNumber: "05035200073")
-        studentsDb.addStudent(firstName: "Kobi4", lastName: "Or4", idStudent: "2004358654", phoneNumber: "05035200074")
+
         
         for (index, element) in studentsDb.students.enumerated()
-        {
-            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-            print("The Name of the \(index) student is:\(element!.fName) \(element!.lName) \(element!.id) \(element!.phoneNum) ");
-
-          }
+        { print("The Name of the \(index) student is:\(element!.fName) \(element!.lName) \(element!.id) \(element!.phoneNum) ");}
         
+        if (testFunctionality(stuDb: studentsDb))
+        {
+            
+            print("Test succesdded")
+        }
     }
     
-    func testPerformanceExample() {
+    func testPerformanceExample()
+    {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
     
+    func testFunctionality(stuDb:StudentDataBase)->(Bool)
+    {
+        for (_,value) in stuDb.students.enumerated()
+        {
+            
+            if (value!.id=="2004358652")
+            {
+                if (stuDb.deleteStudent(ids:"2004358652"))
+                                {
+                    print("2004358652 was deleted");
+                }
+                
+            }
+            for (index, element) in stuDb.students.enumerated()
+            {
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                print("The Name of the \(index) student is:\(element!.fName) \(element!.lName) \(element!.id) \(element!.phoneNum) ");
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                
+            }
+        }
+        return true;
+        
+    }
+    
+    
+    func testUpdate(st:Student)->(Bool)
+    {
+        for (_,value) in studentsDb.students.enumerated()
+        {
+            
+            if (value!.id=="2004358652")
+            {
+                if (studentsDb.deleteStudent(ids:"2004358652"))
+                {
+                    print("2004358652 was deleted");
+                }
+                
+            }
+            for (index, element) in studentsDb.students.enumerated()
+            {
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                print("The Name of the \(index) student is:\(element!.fName) \(element!.lName) \(element!.id) \(element!.phoneNum) ");
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                
+            }
+        }
+        return true;
+        
+    }
+
+
 }
-
-
