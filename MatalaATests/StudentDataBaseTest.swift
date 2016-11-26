20e249bab4233e09c72c2fe7b4c0ac2415f5f32c
KobiOR <admin@IMGOSX.local>
2016-11-26 15:29:52 +0200

    update

--- MatalaATests/StudentDataBaseTest.swift
+++ MatalaATests/StudentDataBaseTest.swift
@@ -9,13 +9,12 @@
 import XCTest
 
 class StudentDataBaseTest: XCTestCase {
-        
+    let studentsDb=StudentDataBase.getInstance;
+    let studentsDb2=StudentDataBase.getInstance;
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
         
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
@@ -31,6 +30,11 @@ class StudentDataBaseTest: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testSingelton() ->(Bool){
+        if (studentsDb===studentsDb2){print("Singelton class succeeded!");return true;}
+        return false;
     }
     
 }
